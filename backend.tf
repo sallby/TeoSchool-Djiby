ceb7c91fbb27a9bd1b88ccb0505d248a77dcf274
sallby <144801648+sallby@users.noreply.github.com>
2023-10-20 17:11:19 +0200

    update backend

--- backend.tf
+++ backend.tf
@@ -4,8 +4,5 @@ terraform {
     storage_account_name = "djibystorageaccount"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    subscription_id = #{AZURE_SUBSCRIPTION_ID}#
-    tenant_id = #{AZURE_TENANT_ID}#
-    access_key =  #{STORAGE_KEY}#
   }
 }
