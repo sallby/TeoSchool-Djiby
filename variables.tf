ac13f1b9be20fe5e8296957713a2d270d38c1857
sallby <144801648+sallby@users.noreply.github.com>
2024-03-06 20:27:15 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -43,7 +43,7 @@ variable "dns_prefix" {
 variable "kubernetes_version" {
   description = "Version de Kubernetes"
   type        = string
-  default     = "1.22"
+  default     = "1.29.0"
 }
 
 variable "node_count" {
